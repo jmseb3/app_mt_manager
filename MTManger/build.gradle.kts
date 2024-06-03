c7e1db14233068b94518cc8beb38b2be25bb6489
jmseb3 <jmseb3@naver.com>
2024-06-03 16:27:33 +0900

    Fix VersionInfo

--- MTManger/build.gradle.kts
+++ MTManger/build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.hilt).apply(false)
+    alias(libs.plugins.room).apply(false)
 }
