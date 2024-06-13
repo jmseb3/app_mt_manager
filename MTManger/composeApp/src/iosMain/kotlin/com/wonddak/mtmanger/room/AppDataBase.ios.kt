029115463ce9b6f2cedb6ff8e83b36d10199101b
jmseb3 <jmseb3@naver.com>
2024-06-13 12:12:19 +0900

    ROOM 버전 최신화

--- MTManger/composeApp/src/iosMain/kotlin/com/wonddak/mtmanger/room/AppDataBase.ios.kt
+++ MTManger/composeApp/src/iosMain/kotlin/com/wonddak/mtmanger/room/AppDataBase.ios.kt
@@ -1,21 +1,10 @@
 package com.wonddak.mtmanger.room
 
-import androidx.room.Room
-import androidx.room.RoomDatabase
 import kotlinx.cinterop.ExperimentalForeignApi
 import platform.Foundation.NSFileManager
-import platform.Foundation.NSHomeDirectory
 import platform.Foundation.NSLibraryDirectory
 import platform.Foundation.NSURL
 import platform.Foundation.NSUserDomainMask
-
-//actual fun getDatabaseBuilder(): RoomDatabase.Builder<AppDatabase> {
-//    val dbFilePath = NSHomeDirectory() + "/" + DB_NAME
-//    return Room.databaseBuilder<AppDatabase>(
-//        name = dbFilePath,
-//        factory =  { AppDatabase::class.instantiateImpl() }
-//    )
-//}
 
 @OptIn(ExperimentalForeignApi::class)
 internal fun providePath() :String {
