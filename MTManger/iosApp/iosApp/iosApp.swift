75b90738656e2ddcdc847fffb271704d825f9dc1
jmseb3 <jmseb3@naver.com>
2024-07-01 23:11:01 +0900

    [IOS] FireBase init

--- MTManger/iosApp/iosApp/iosApp.swift
+++ MTManger/iosApp/iosApp/iosApp.swift
@@ -1,5 +1,7 @@
 import UIKit
 import ComposeApp
+import FirebaseCore
+import GoogleMobileAds
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -9,6 +11,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        
+        // Use Firebase library to configure APIs.
+        FirebaseApp.configure()
+        // Initialize the Google Mobile Ads SDK.
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         if let window = window {
             window.rootViewController = MainKt.MainViewController()
