33f7e7f34e51b9006d00d9f71b30bd330f1064d4
jmseb3 <jmseb3@naver.com>
2024-08-02 09:49:16 +0900

    Release (3.0.4)

--- MTManger/composeApp/build.gradle.kts
+++ MTManger/composeApp/build.gradle.kts
@@ -102,8 +102,8 @@ android {
         applicationId = "com.wonddak.mtmanger"
         minSdk = 24
         targetSdk = 34
-        versionCode = 16
-        versionName = "3.0.3"
+        versionCode = 17
+        versionName = "3.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
