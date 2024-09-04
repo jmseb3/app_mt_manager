ca263e579651dca7dd332baecad35e6998be9cf1
jmseb3 <jmseb3@naver.com>
2024-09-04 10:05:20 +0900

    Fix Call Intent Error

--- MTManger/composeApp/src/androidMain/kotlin/com/wonddak/mtmanger/util/DeviceActionHelper.android.kt
+++ MTManger/composeApp/src/androidMain/kotlin/com/wonddak/mtmanger/util/DeviceActionHelper.android.kt
@@ -2,6 +2,7 @@ package com.wonddak.mtmanger.util
 
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.net.Uri
 import org.koin.java.KoinJavaComponent
 
@@ -11,7 +12,9 @@ actual class DeviceActionHelper {
         context.startActivity(
             Intent(
                 Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber")
-            )
+            ).apply {
+                flags = FLAG_ACTIVITY_NEW_TASK
+            }
         )
     }
 
